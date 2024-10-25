a41905158f945b26273673d8c0054ed612a6e050
ibharatsaini <sainib601@gmail.com>
2024-10-25 15:53:54 +0530

    initial

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+
+
+const app = express()
+
+
+
+app.use("/",()=>{
+    setInterval(()=>{
+        fetch('https://movie-booking-yj2w.onrender.com/')
+    },20000)
+    res.status(200).json({
+        message:`Up and running`
+    })
+})
+
+app.listen(process.env.PORT || '8080', ()=>{
+     console.log(`Server running on port`,process.env.PORT || '8080')
+})
+
+
+
+module.exports = app
